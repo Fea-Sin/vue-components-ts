ea3a486044398c917dd5e3f8f6428f0f39a08ea4
Fea-Sin <1210992614@qq.com>
2021-08-12 15:33:01 +0800

    提交代码

--- src/views/Vface.vue
+++ src/views/Vface.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="TBox">
     <div>Ace</div>
-    <!-- <MyAce width="100%" height="400px" /> -->
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
-// import VFAce from "vf-vue-ace";
-// eslint-disable-next-line
-// const MyAce = require("vf-vue-ace");
-
 @Component({
-  components: {
-    // MyAce,
-  },
+  components: {},
 })
 export default class AceDemo extends Vue {}
 </script>
