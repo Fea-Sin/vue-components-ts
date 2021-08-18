84a95770cfbf79e3d91c557fe5fd9210f9f9273e
Fea-Sin <1210992614@qq.com>
2021-08-18 16:38:38 +0800

    提交代码

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -13,6 +13,20 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
+import "ace-builds/src-min-noconflict/ext-searchbox";
+import "ace-builds/src-min-noconflict/ext-language_tools";
+
+const Theme = ["monokai", "solarized_light", "solarized_dark"];
+const Mode = ["javascript", "mysql", "java", "golang"];
+
+Theme.forEach((theme) => {
+  require(`ace-builds/src-noconflict/theme-${theme}`);
+});
+Mode.forEach((lang) => {
+  require(`ace-builds/src-noconflict/mode-${lang}`);
+  require(`ace-builds/src-noconflict/snippets/${lang}`);
+});
+
 @Component
 export default class Editor extends Vue {}
 </script>
