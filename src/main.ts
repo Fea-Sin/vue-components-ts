a8a925628f3d5e2f8cf633d4b576e8d6d411520b
Fea-Sin <1210992614@qq.com>
2021-08-26 14:54:15 +0800

    提交代码

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,19 @@ import iView from "iview";
 import "iview/dist/styles/iview.css";
 import "./assets/css/index.less";
 
+import VFUI from "vf-vue-ui";
+
 import VFACE from "vf-vue-ace";
+import "vf-vue-ui/lib/theme/index.css";
 
 Vue.config.productionTip = false;
 
 Vue.use(iView);
 Vue.use(VFACE);
+Vue.use(VFUI);
+
+console.log("VFUI version----", VFUI.version);
+Vue.component("MyAlert", VFUI.Alert);
 
 const VM = new Vue({
   router,
--- src/views/VFUI/alert.vue
+++ src/views/VFUI/alert.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="TBox">
-    <div>ALERT</div>
+    <div class="box">ALERT</div>
+    <div class="box">
+      <vf-alert title="提示文案55" type="success" />
+    </div>
+    <div class="box">
+      <vf-alert title="重要提示" type="error" effect="dark" />
+    </div>
+    <div class="box">
+      <vf-alert
+        title="警告文本"
+        type="warning"
+        description="这里会有一些文本提示"
+      />
+    </div>
+    <div class="box">
+      <MyAlert title="8778787878" />
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -8,4 +24,9 @@ import Vue from "vue";
 
 export default Vue.extend({});
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.box {
+  padding: 16px;
+  width: 500px;
+}
+</style>
